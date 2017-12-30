02ac1e5b440763624f7f7add4dda107da7d3098d
Abdulrahim Klis <abudu.klis@hotmail.com>
2017-12-30 17:38:30 +0000

    first commit

---
+++ src/app/services/item.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from "angularfire2/firestore";
+import {Observable} from "rxjs/Observable";
+import { Item } from '../models/Item';
+
+
+@Injectable()
+export class ItemService {
+  itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+
+
+  constructor(private afs: AngularFirestore) {
+    this.items = this.afs.collection('items').valueChanges();
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+}
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+//link of the firebase: https://console.firebase.google.com/project/gs1prod-576c7/database/firestore/data~2Fitems~2FLCO0NDRfL6tN2vQK6EXn
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA7Q5yCJvcojl6Xd9xRB9AIyyJV3FUOq6I",
+    authDomain: "gs1prod-576c7.firebaseapp.com",
+    databaseURL: "https://gs1prod-576c7.firebaseio.com",
+    projectId: "gs1prod-576c7",
+    storageBucket: "gs1prod-576c7.appspot.com",
+    messagingSenderId: "944555371798"
+  }
 };
